445f867eb291c8bdc8ddbaa98064fbde995cb26a
Leandre Outers <outers.leandre@gmail.com>
2020-10-23 11:30:47 +0100

    Add _get_balance function, str format coercion
    str format coercion/conversion to minimize typo assertion error.

--- portfolio.py
+++ portfolio.py
@@ -8,19 +8,23 @@ API_SOURCES = {'Kraken', 'Bitfinex'}
 class Portfolio(object):
 
     def __init__(self, base_fiat: str, base_crypto: str, api_sources: set):
+        base_fiat = base_fiat.upper()
         assert base_fiat in BASE_FIATS
         self.base_fiat = base_fiat
 
+        base_crypto = base_crypto.upper()
         assert base_crypto in BASE_CRYPTOS
         self.base_crypto = base_crypto
 
+        api_sources = {x.capitalize() for x in api_sources}
         assert api_sources in API_SOURCES
         self.api_sources = api_sources
 
         self.balance = {}
 
     def _get_balance(self):
-        pass
+        for api_source in self.api_sources:
+            self.balance[api_source] = api_source.get_balance()
 
     def get_last(self):
         pass
