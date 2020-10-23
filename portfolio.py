da33c7bc550db71e84c17c7c48428b59e1daa3e2
Leandre Outers <outers.leandre@gmail.com>
2020-10-23 10:59:20 +0100

    Change class var to constants, add assertions
    Class variables base_fiats, base_cryptos, api_sources changed to global
    constants.
    Added assertions to ensure constructor arguments are in the
    aforementioned constants.

--- portfolio.py
+++ portfolio.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
 
+BASE_FIATS = {'USD', 'EUR', 'GBP'}
+BASE_CRYPTOS = {'BTC', 'ETH'}
+API_SOURCES = {'Kraken', 'Bitfinex'}
+
 
 class Portfolio(object):
-    base_fiats = ['USD', 'EUR', 'GBP']
-    base_cryptos = ['BTC', 'ETH']
-    api_sources = ['Kraken', 'Bitfinex']
 
-    def __init__(self, base_fiat: str, base_crypto: str, apis_source: str):
+    def __init__(self, base_fiat: str, base_crypto: str, api_sources: set):
+        assert base_fiat in BASE_FIATS
         self.base_fiat = base_fiat
+
+        assert base_crypto in BASE_CRYPTOS
         self.base_crypto = base_crypto
-        self.apis_source = apis_source
+
+        assert api_sources in API_SOURCES
+        self.api_sources = api_sources
+
         self.balance = {}
 
     def _get_balance(self):
