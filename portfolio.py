6e66638b21ffe5f8f0b3ecee3c70bdb9303d5836
Leandre Outers <outers.leandre@gmail.com>
2020-11-12 17:08:03 +0000

    Add get_balance feature using Bfx & Kraken APIs
    Portofolio.get_balance() integrates the return value of
    Bitfinex.get_balance() and Kraken.get_blance().

--- portfolio.py
+++ portfolio.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from apis import Kraken, Bitfinex
 
 BASE_FIATS = {'USD', 'EUR', 'GBP'}
 BASE_CRYPTOS = {'BTC', 'ETH'}
@@ -7,7 +8,15 @@ API_SOURCES = {'Kraken', 'Bitfinex'}
 
 class Portfolio(object):
 
-    def __init__(self, base_fiat: str, base_crypto: str, api_sources: set):
+    def __init__(self, apis: set, base_fiat: str, base_crypto: str = ''):
+        ''' Initialize Portfolio with basic information and checking.
+
+        Args:
+        - apis: {(<API Source>, <path to key>)} eg {('Kraken', 'kraken.key')}
+        - base_fiat: currency in which holdings are reported
+        - base_crypto: digital asset in which holdings are reported
+        '''
+
         base_fiat = base_fiat.upper()
         assert base_fiat in BASE_FIATS
         self.base_fiat = base_fiat
@@ -16,15 +25,18 @@ class Portfolio(object):
         assert base_crypto in BASE_CRYPTOS
         self.base_crypto = base_crypto
 
-        api_sources = {x.capitalize() for x in api_sources}
-        assert api_sources in API_SOURCES
+        for api_source in apis:
+            assert api_source[0].capitalize() in API_SOURCES
+        api_sources = [eval(x[0].capitalize())(x[1]) for x in apis]
         self.api_sources = api_sources
 
         self.balance = {}
 
-    def _get_balance(self):
+    def get_balance(self):
+        ''' Get balance of holdings from different APIs.'''
         for api_source in self.api_sources:
-            self.balance[api_source] = api_source.get_balance()
+            self.balance[type(api_source).__name__] = api_source.get_balance(
+                                       self.base_fiat, self.base_crypto)
 
     def get_last(self):
         pass
