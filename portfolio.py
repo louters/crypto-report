b6606b626f8db3831b0e8d9318e1ebbee1b5984b
Leandre Outers <outers.leandre@gmail.com>
2021-01-12 18:54:20 +0000

    Simply code: ref_api set in constructor
    Previously ref_api could be set at different parts of the code, which made it hard to maintain. Now, it is set in __init__, simplyfing management of ref_api and code maintenant.

--- portfolio.py
+++ portfolio.py
@@ -10,13 +10,15 @@ API_SOURCES = {'Kraken', 'Bitfinex', 'Etherscan', 'Blockchain'}
 
 
 class Portfolio(object):
-    def __init__(self, apis: set, base_fiat: str, base_crypto: str = ''):
+    def __init__(self, apis: set, base_fiat: str, base_crypto: str='',
+                 ref_api: str='') -> None:
         ''' Initialize Portfolio with basic information and checking.
 
         Args:
         - apis: {(<API Source>, <path to key>)} eg {('Kraken', 'kraken.key')}
-        - base_fiat: currency in which holdings are reported
-        - base_crypto: digital asset in which holdings are reported
+        - base_fiat: Currency in which holdings are reported
+        - base_crypto: Digital asset in which holdings are reported
+        - ref_api: For non-exchange APIs, the prices from ref_api will be used
         '''
 
         base_fiat = base_fiat.upper()
@@ -31,8 +33,13 @@ class Portfolio(object):
         for api_source in apis:
             assert api_source[0].capitalize() in API_SOURCES
 
+        if ref_api:
+            self.ref_api = ref_api.capitalize()
+            assert self.ref_api in API_SOURCES
+
         # Initialize each API (one file only)
-        api_sources = [eval(api[0].capitalize())(api[1]) for api in apis if len(api) == 2]
+        api_sources = [eval(api[0].capitalize())(api[1]) 
+                       for api in apis if len(api) == 2]
 
         # Case where constructor has two files
         for api in apis:
@@ -40,14 +47,11 @@ class Portfolio(object):
                 api_sources.append(eval(api[0].capitalize())(api[1], api[2]))
         self.api_sources = api_sources
 
-        self.balance = pd.DataFrame(columns = ['Asset', 'Amount', 'price_f', 'price_c'])
+        columns = ['Asset', 'Amount', 'price_f', 'price_c']
+        self.balance = pd.DataFrame(columns = columns)
 
-    def get_balance(self, ref_api: str='') -> pd.DataFrame:
-        ''' Get balance of holdings from different APIs.
-
-        Args:
-        - ref_api: For non-exchange APIs, the prices from ref_api will be used.
-        '''
+    def get_balance(self) -> pd.DataFrame:
+        ''' Get balance of holdings from different APIs.'''
         
         # Get balance for different APIs
         for api_source in self.api_sources:
@@ -55,8 +59,9 @@ class Portfolio(object):
             if api_source.__class__.__name__ != 'Etherscan' \
             and api_source.__class__.__name__ != 'Blockchain':
                 self.balance = pd.merge(
-                        api_source.get_balance(self.base_fiat, self.base_crypto),
-                        self.balance, how='outer')
+                                        api_source.get_balance(self.base_fiat,
+                                                           self.base_crypto),
+                                        self.balance, how='outer')
             # Case for BTC/ETH addresses
             else:
                 self.balance = pd.merge(api_source.get_balance(), self.balance,
@@ -66,24 +71,14 @@ class Portfolio(object):
         self.balance.set_index(['API', 'Asset'], inplace=True)
 
         # Setting price_c & price_f for Etherscan & Blockchain Explorer
-        if 'Etherscan' in self.balance.index \
-        or 'Blockchain' in self.balance.index:
-            if not ref_api:
-                self.ref_api = 'Kraken'
-                print('WARNING - reference API set to Kraken')
-            # Check ref_api exists in dataframe
-            else:
-                assert ref_api != 'Etherscan'
-                assert ref_api != 'Blockchain'
-                self.ref_api = ref_api
-
         if 'Etherscan' in self.balance.index:
-            self.set_address_prices('Etherscan', self.ref_api)
+            self.set_address_prices('Etherscan')
         if 'Blockchain' in self.balance.index:
-            self.set_address_prices('Blockchain', self.ref_api)
+            self.set_address_prices('Blockchain')
 
         # Add Fiat and Crypto Values columns
-        self.balance['value_f'] = self.balance['Amount'] * self.balance['price_f']
+        self.balance['value_f'] = (self.balance['Amount'] 
+                                   * self.balance['price_f'])
         if self.base_crypto:
             self.balance['value_c'] = self.balance['Amount'] * self.balance['price_c']
         else:
@@ -96,21 +91,19 @@ class Portfolio(object):
 
         return self.balance
 
-    def print_balance(self, ref_api: str='') -> None:
-        ''' Print a pretty balance. 
+    def print_balance(self) -> None:
+        ''' Print a pretty balance.'''
 
-        Args:
-        - ref_api: For non-exchange APIs, the price from ref_api will be used.
-        '''
         format = ',.2f'
 
         # Check balance exists
         if self.balance.empty:
-            self.get_balance(ref_api)
+            self.get_balance()
 
         # Print totals
         print()
-        print(f'Date: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}', end='\n\n')
+        print(f'Date: {datetime.now().strftime("%d/%m/%Y %H:%M:%S")}',
+              end='\n\n')
         print(f'Total in {self.base_fiat}: {self.total_fiat:{format}}')
         if self.base_crypto:
             print(f'Total in {self.base_crypto}: {self.total_crypto:{format}}')
@@ -149,12 +142,11 @@ class Portfolio(object):
                                         self.balance['value_f'] >= threshold]
         return self.simple_balance
 
-    def set_address_prices(self, api: str, ref_api: str) -> None:
+    def set_address_prices(self, api: str) -> None:
         ''' Set ETH or BTC address price(s)
 
         Args:
         - api: Either 'Etherscan' or 'Blockchain' (explorer)
-        - ref_api: the API from which the price will be used
         '''
         if api == 'Etherscan':
             crypto = 'ETH'
@@ -164,34 +156,26 @@ class Portfolio(object):
             raise Exception(f'{api} is not implemented.')
 
         try:
-            self.balance.loc[(api, crypto)]['price_f'] = self.balance.loc[(ref_api, crypto)]['price_f']
+            self.balance.loc[(api, crypto)]['price_f'] = (
+                self.balance.loc[(self.ref_api, crypto)]['price_f'])
             if self.base_crypto == crypto:
-                    self.balance.loc[(api, crypto)]['price_c'] = 1
+                self.balance.loc[(api, crypto)]['price_c'] = 1
             else:
-                    self.balance.loc[(api, crypto)]['price_c'] = self.balance[(ref_api, crypto)]['price_c']
+                self.balance.loc[(api, crypto)]['price_c'] = (
+                    self.balance[(self.ref_api, crypto)]['price_c'])
         except KeyError:
-            ref_api_session = eval(ref_api)()
-            price_f, price_c = ref_api_session.get_price(crypto, self.base_fiat, self.base_crypto)
+            ref_api_session = eval(self.ref_api)()
+            price_f, price_c = (
+                ref_api_session.get_price(
+                    crypto, self.base_fiat, self.base_crypto))
             self.balance.loc[(api, crypto)]['price_f'] = price_f
             self.balance.loc[(api, crypto)]['price_c'] = price_c
 
-    def get_risk(self, ref_api: str) -> None:
+    def get_risk(self) -> None:
         ''' Print a set of risk measures of portfolio.
 
         N.B.: No backfilling is performed.
-        Args:
-        - ref_api: for non-exchange APIs, the API from which history will
-        downloaded from
         '''
-
-        # Checks for ref api
-        if ref_api and not hasattr(self, 'ref_api'):
-            self.ref_api = ref_api
-        elif ref_api and self.ref_api != ref_api:
-            print(f'ref_api is already set as {self.ref_api} and will not be changed')
-        elif not ref_api and not hasattr(self, 'ref_api'):
-            self.ref_api = 'Kraken'
-            print('WARNING - reference API set to Kraken')
 
         # Check simple balance exists
         if not hasattr(self, 'simple_balance'):
@@ -204,7 +188,8 @@ class Portfolio(object):
             and asset not in BASE_FIATS:
                 ticker = asset + self.base_fiat
                 method = f"{api}()"
-                df[api + '-' + asset] = eval(method).get_history(ticker)['close']
+                idx = api + '-' + asset
+                df[idx] = eval(method).get_history(ticker)['close']
 
         # DROP NA fo same length of time series (no backfilling)
         print()
@@ -224,13 +209,15 @@ class Portfolio(object):
 
         # Compute 20-days daily volatility in fiat value
         if 'Etherscan' in self.simple_balance.index:
-            vols.loc[('Etherscan', 'ETH'), :] = vols.loc[(ref_api, 'ETH'), :]
+            vols.loc[('Etherscan', 'ETH'), :] = (
+                vols.loc[(self.ref_api, 'ETH'), :])
         if 'Blockchain' in self.simple_balance.index:
-            vols.loc[('Blockchain', 'BTC'), :] = vols.loc[(ref_api, 'BTC'), :]
+            vols.loc[('Blockchain', 'BTC'), :] = (
+                vols.loc[(self.ref_api, 'BTC'), :])
 
         vols['vol_fiat'] = vols['vol_pct'] * self.simple_balance['value_f']
         print(f'Daily volatility over last 20 days: ', end='')
-        print(f'{vols["vol_fiat"].sum():,.2f}', end=2*'\n')
+        print(f'{vols["vol_fiat"].sum():,.2f} {self.base_fiat}', end=2*'\n')
         print(vols, end=2*'\n')
 
         # Set return values for non-exchange holdings
@@ -246,30 +233,38 @@ class Portfolio(object):
         tmp_7d = ret_7d.copy()
         for idx in self.simple_balance.index:
             idx_ret = '-'.join(idx)
-            tmp[idx_ret] = ret[idx_ret] * self.simple_balance.loc[idx, 'value_f']
-            tmp_7d[idx_ret] = ret_7d[idx_ret] * self.simple_balance.loc[idx, 'value_f']
+            tmp[idx_ret] = (
+                ret[idx_ret] * self.simple_balance.loc[idx, 'value_f'])
+            tmp_7d[idx_ret] = (
+                ret_7d[idx_ret] * self.simple_balance.loc[idx, 'value_f'])
 
         # Worst/Best days/weeks
         totals = tmp.sum(axis=1).sort_values()
+        print('N.B.: The following uses historical returns applied to today\'s portfolio')
         print(f'Worst day ', end='')
         print(f'({totals.iloc[[0]].index[0]:%d/%m/%Y}): ', end='')
-        print(f'{totals.iloc[0]:,.2f} {self.base_fiat}')
+        print(f'{totals.iloc[0]:,.2f} {self.base_fiat} ', end='')
+        print(f'({totals.iloc[0]/self.total_fiat:.2%})')
         print(f'Best day ', end='')
         print(f'({totals.iloc[[-1]].index[0]:%d/%m/%Y}): ', end='')
-        print(f'{totals.iloc[-1]:,.2f} {self.base_fiat}', end=2*'\n')
+        print(f'{totals.iloc[-1]:,.2f} {self.base_fiat} ', end='')
+        print(f'({totals.iloc[-1]/self.total_fiat:.2%})', end=2*'\n')
 
         totals_7d = tmp_7d.sum(axis=1).sort_values()
         print(f'Worst week ', end='')
         print(f'({totals_7d.iloc[[0]].index[0]:%d/%m/%Y}): ', end='')
-        print(f'{totals_7d.iloc[0]:,.2f} {self.base_fiat}')
+        print(f'{totals_7d.iloc[0]:,.2f} {self.base_fiat} ', end='')
+        print(f'({totals_7d.iloc[0]/self.total_fiat:.2%})')
         print(f'Best week ', end='')
         print(f'({totals_7d.iloc[[-1]].index[0]:%d/%m/%Y}): ', end='')
-        print(f'{totals_7d.iloc[-1]:,.2f} {self.base_fiat}', end=2*'\n')
+        print(f'{totals_7d.iloc[-1]:,.2f} {self.base_fiat} ', end='')
+        print(f'({totals_7d.iloc[-1]/self.total_fiat:.2%})', end=2*'\n')
         
         # Expected Shortfall 1-day & 7-days at 97.5%
         es_1d = totals.iloc[:int(len(totals) * 0.025)].mean()
-        print(f'1-day Expected Shortfall @ 97.5%: {es_1d:,.2f}')
+        print(f'1-day Expected Shortfall @ 97.5%: {es_1d:,.2f} {self.base_fiat}')
         es_7d = totals_7d.iloc[:int(len(totals_7d) * 0.025)].mean()
-        print(f'7-day Expected Shortfall @ 97.5%: {es_7d:,.2f}', end=2*'\n')
+        print(f'7-day Expected Shortfall @ 97.5%: {es_7d:,.2f} {self.base_fiat}',
+              end=2*'\n')
 
         return totals, totals_7d
